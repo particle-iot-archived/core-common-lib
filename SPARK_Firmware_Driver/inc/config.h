4c620b4aa3e939b0de4adaafd55fdd42793eeef3
Mohit <mohit@ubuntu.(none)>
2014-03-03 14:53:36 -0600

    enabled debug

--- SPARK_Firmware_Driver/inc/config.h
+++ SPARK_Firmware_Driver/inc/config.h
@@ -8,11 +8,13 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#if !defined(RELEASE_BUILD) && !defined(DEBUG_BUILD)
-#warning  "Defaulting to Release Build"
-#define RELEASE_BUILD
-#undef  DEBUG_BUILD
-#endif
+#define DEBUG_BUILD
+
+//#if !defined(RELEASE_BUILD) && !defined(DEBUG_BUILD)
+//#warning  "Defaulting to Release Build"
+//#define RELEASE_BUILD
+//#undef  DEBUG_BUILD
+//#endif
 //#define USE_ONLY_PANIC // Define to remove all Logging and only have Panic
 
 // define to include __FILE__ information within the debug output
