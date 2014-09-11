0ed0547a7e597874fb0224a19ed6901007a16668
Matthew McGowan <mat.mcgowan+github+profile@gmail.com>
2014-09-12 01:31:31 +0200

    SPARK_PRODUCT_ID can be passed on the commandline. When spid==2, targets STM32F10x_HD.(256k internal flash.)

--- SPARK_Firmware_Driver/inc/platform_config.h
+++ SPARK_Firmware_Driver/inc/platform_config.h
@@ -27,6 +27,8 @@
 #ifndef __PLATFORM_CONFIG_H
 #define __PLATFORM_CONFIG_H
 
+#include <limits.h>
+
 /* Uncomment the line below to enable WLAN, WIRING, SFLASH and RTC functionality */
 #define SPARK_WLAN_ENABLE
 #define SPARK_WIRING_ENABLE
@@ -194,6 +196,25 @@
 #define SYSTICK_IRQ_PRIORITY				13	//CORTEX_M3 Systick Interrupt
 #define SVCALL_IRQ_PRIORITY					14	//CORTEX_M3 SVCall Interrupt
 #define PENDSV_IRQ_PRIORITY					15	//CORTEX_M3 PendSV Interrupt
+
+#ifndef SPARK_PRODUCT_ID
+#define SPARK_PRODUCT_ID 0
+#warning "SPARK_PRODUCT_ID not defined, assuming 0"
+#endif
+
+
+#define PREPSTRING2(x) #x
+#define PREPSTRING(x) PREPSTRING2(x)
+
+#if SPARK_PRODUCT_ID < 2
+#define INTERNAL_FLASH_SIZE     (0x20000)
+#elif SPARK_PRODUCT_ID == 2
+    #define INTERNAL_FLASH_SIZE (0x40000)
+#else
+    #pragma message "SPARK_PRODUCT_ID is " PREPSTRING(SPARK_PRODUCT_ID)
+    #error "Unknown SPARK_PRODUCT_ID"
+#endif
+
 
 /* Exported functions ------------------------------------------------------- */
 
